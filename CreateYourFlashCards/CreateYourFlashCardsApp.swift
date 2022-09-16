b2f912622c569b883a42568f1d5ee7a31b843195
ibrerdogan <94644110+ibrerdogan@users.noreply.github.com>
2022-09-16 11:21:27 +0300

    Initial Commit

---
+++ CreateYourFlashCards/CreateYourFlashCardsApp.swift
@@ -0,0 +1,17 @@
+//
+//  CreateYourFlashCardsApp.swift
+//  CreateYourFlashCards
+//
+//  Created by İbrahim Erdogan on 16.09.2022.
+//
+
+import SwiftUI
+
+@main
+struct CreateYourFlashCardsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CreateYourFlashCardsUITests/CreateYourFlashCardsUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  CreateYourFlashCardsUITestsLaunchTests.swift
+//  CreateYourFlashCardsUITests
+//
+//  Created by İbrahim Erdogan on 16.09.2022.
+//
+
+import XCTest
+
+class CreateYourFlashCardsUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
